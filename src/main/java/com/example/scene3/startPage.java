9c252aa8eec3a8b873e2c626ecc219e81ea3eb80
mrimmoyee <142920390+mrimmoyee@users.noreply.github.com>
2024-06-01 21:59:59 +0600

    start page buttons

--- src/main/java/com/example/scene3/startPage.java
+++ src/main/java/com/example/scene3/startPage.java
@@ -8,6 +8,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
@@ -61,6 +63,7 @@ public class startPage extends Application {
         // Call the start_new method with the stage parameter
         hc.start_new(st);
     }
+
 
     @FXML
     public void exitApplication(MouseEvent event) {
