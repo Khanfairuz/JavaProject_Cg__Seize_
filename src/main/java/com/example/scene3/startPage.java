c58fbd096ffec4017d975bfd7d4c04b7ba08f8a1
nufsatfarooque <140724738+nufsatfarooque@users.noreply.github.com>
2024-06-02 13:55:35 +0600

    Edits for obstacles

--- src/main/java/com/example/scene3/startPage.java
+++ src/main/java/com/example/scene3/startPage.java
@@ -61,11 +61,15 @@ public class startPage extends Application {
         HelloController hc = new HelloController();
 
         // Call the start_new method with the stage parameter
-        hc.start_new(st);
+        //hc.start_new(st);
         HelloController3 hc3 = new HelloController3();
 
         // Call the start_new method with the stage parameter
         //hc3.start_new_3(st, 0, 0, 0);
+        HelloController2 hc2 = new HelloController2();
+
+        // Call the start_new method with the stage parameter
+        hc2.start_new_2(st, 0, 0, 0);
     }
 
 
